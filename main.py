bd67d8c5aa46bbd6d9d4d102388822f46908e5c7
ai-zerocoder-ai <ai.zerocoder.ai@gmail.com>
2024-10-13 23:02:02 +0300

    add new const

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Play the game")
+icon = pygame.image.load("img/42648.jpg")
+pygame.display.set_icon(icon)
 
 
 
